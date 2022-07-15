cad503eb518f227ba3095a9f83f3935fda0f2995
Jose <ls.joseignacio@gmail.com>
2022-07-15 13:17:14 +0200

    Implementada posicion inicial del mapa a partir de localizacion del dispositivo

--- src/containers/Home/Home.jsx
+++ src/containers/Home/Home.jsx
@@ -1,9 +1,16 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import GoogleMapReact from 'google-map-react';
 import MyMarker from './MyMarker';
 import useSupercluster from 'use-supercluster';
 
+/**
+ * Converts an array of objects with name, lat and lng properties into an array
+ * of GeoJSON elements
+ *
+ * @param {*} arr Array of markers with properties name, lat and lng
+ * @returns Array of GeoJSON elements from the provided points
+ */
 const parseToGeoJSON = arr => {
   return arr.map(point => {
     return {
@@ -17,69 +24,97 @@ const parseToGeoJSON = arr => {
   });
 };
 
+// Points to draw, should be substituted by the retrieved from the API
 const MARKERS = [
   { name: 'Madrid', lat: 40.41, lng: -3.71 },
   { name: 'Alicante', lat: 38.34, lng: -0.49 },
   { name: 'Vigo', lat: 42.23, lng: -8.71 },
 ];
 
+// Google maps API KEY
 const API_KEY = 'AIzaSyB_lyylykWMCpfn9_tlFpoN9mqWNVIV-a8';
 
-const points = parseToGeoJSON(MARKERS);
-
-//console.log(points);
+// GeoJSON points
+const GeoJSONPoints = parseToGeoJSON(MARKERS);
 
 const Home = () => {
   const [bounds, setBounds] = useState(null);
   const [zoom, setZoom] = useState(10);
 
+
+  const [userLocation, setUserLocation] = useState({}); // initial center of the map
+  const [userPermission, setUserPermission] = useState('pending'); // controls whether the user has accepted location permissions
+
   const { clusters, supercluster } = useSupercluster({
-    points,
+    points: GeoJSONPoints,
     bounds,
     zoom,
     options: { radius: 150, maxZoom: 20 },
   });
 
+  /**
+   * Gets the physical user location to center the map
+   * If the user denies permission uses the first point
+   * in the array of markers
+   */
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      data => {
+        setUserPermission('granted');
+        setUserLocation({
+          lat: data.coords.latitude,
+          lng: data.coords.longitude,
+        });
+      },
+      () => {
+        setUserPermission('denied');
+        setUserLocation(MARKERS[0]);
+      }
+    );
+  }, []);
+
   return (
     <div style={{ height: '400px' }}>
-      <GoogleMapReact
-        onChange={({ zoom, bounds }) => {
-          setZoom(zoom);
-          setBounds([
-            bounds.nw.lng,
-            bounds.se.lat,
-            bounds.se.lng,
-            bounds.nw.lat,
-          ]);
-        }}
-        defaultZoom={2}
-        bootstrapURLKeys={{ key: API_KEY }}
-        defaultCenter={{ lat: MARKERS[0].lat, lng: MARKERS[0].lng }}
-      >
-        {clusters.map(cluster => {
-          const [longitude, latitude] = cluster.geometry.coordinates;
-          const { cluster: isCluster, point_count: pointCount } =
-            cluster.properties;
-          if (isCluster) {
+      {userPermission !== 'pending' && (
+        <GoogleMapReact
+          onChange={({ zoom, bounds }) => {
+            setZoom(zoom);
+            setBounds([
+              bounds.nw.lng,
+              bounds.se.lat,
+              bounds.se.lng,
+              bounds.nw.lat,
+            ]);
+          }}
+          defaultZoom={15}
+          bootstrapURLKeys={{ key: API_KEY }}
+          defaultCenter={userLocation}
+        >
+          {clusters.map(cluster => {
+            const [longitude, latitude] = cluster.geometry.coordinates;
+            const { cluster: isCluster, point_count: pointCount } =
+              cluster.properties;
+            if (isCluster) {
+              return (
+                <MyMarker
+                  key={cluster.properties.name}
+                  lat={latitude}
+                  lng={longitude}
+                  name={pointCount}
+                />
+              );
+            }
             return (
               <MyMarker
                 key={cluster.properties.name}
                 lat={latitude}
                 lng={longitude}
-                name={pointCount}
+                name={cluster.properties.name}
               />
             );
-          }
-          return (
-            <MyMarker
-              key={cluster.properties.name}
-              lat={latitude}
-              lng={longitude}
-              name={cluster.properties.name}
-            />
-          );
-        })}
-      </GoogleMapReact>
+          })}
+        </GoogleMapReact>
+      )}
     </div>
   );
 };
