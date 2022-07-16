a5a56ca04172da3cb319b87e1469d4ac09bba34a
Jose <ls.joseignacio@gmail.com>
2022-07-16 14:39:20 +0200

    Añade pagina de detalle

--- src/containers/Home/Home.jsx
+++ src/containers/Home/Home.jsx
@@ -1,24 +1,23 @@
 import React, { useState } from 'react';
 
-import { Box, Button } from '@chakra-ui/react';
+import { Box } from '@chakra-ui/react';
 
 import GoogleMap from './GoogleMap';
 import Tarjeta from './Tarjeta';
 
 const Home = () => {
   const [selectedSite, setSelectedSite] = useState(undefined);
-  /**
-   * Gets the physical user location to center the map
-   * If the user denies permission uses the first point
-   * in the array of markers
-   */
 
   return (
     <Box display="grid" gridTemplateRows="1fr auto">
       <Box gridRow="2/3" h="calc(100vh - 130px)">
         <GoogleMap setSelectedSite={setSelectedSite} />
       </Box>
-      <Tarjeta gridRow="1/2" selectedSite={selectedSite} setSelectedSite={setSelectedSite} />
+      <Tarjeta
+        gridRow="1/2"
+        selectedSite={selectedSite}
+        setSelectedSite={setSelectedSite}
+      />
     </Box>
   );
 };
