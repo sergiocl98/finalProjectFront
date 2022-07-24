88f09387ee3fce27c90c6887ac687c840a0c0c50
Jose <ls.joseignacio@gmail.com>
2022-07-24 13:43:16 +0200

    Arregla el bug por el cual el boton de book no estaba deshabilitado

--- src/containers/Home/SiteList/Card.jsx
+++ src/containers/Home/SiteList/Card.jsx
@@ -1,19 +1,34 @@
-import { Box, Button, Flex, Heading, HStack, Image, SimpleGrid, Tag, Text } from '@chakra-ui/react';
+import {
+  Box,
+  Button,
+  Flex,
+  Heading,
+  HStack,
+  Image,
+  SimpleGrid,
+  Tag,
+  Text,
+} from '@chakra-ui/react';
 import { MapTrifold } from 'phosphor-react';
 import React, { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import RestaurantDefault from '../../../shared/img/restaurantDefault.jpg';
 
-import { NavLink } from 'react-router-dom';
+import { NavLink, useNavigate } from 'react-router-dom';
 import localService from '../../../services/localService';
 
-const Card = ({ 
-siteData,
+const Card = ({
+  siteData,
   handleSelectSite,
   canClose = true,
   isSearch = false,
-  handleInputReset
+  handleInputReset,
 }) => {
+  const navigate = useNavigate();
+
+  const handleBooking = () => {
+    navigate(`/book/${siteData.properties._id}`);
+  };
   return (
     <Box
       overflow={'hidden'}
@@ -24,75 +39,98 @@ siteData,
       borderWidth="1px"
       bgColor="white"
     >
-    <SimpleGrid columns={2} spacing={5} templateColumns='73% 25%' gap='2%'>
-      <Box>
-      <Flex justifyContent={'space-between'}>
-        <Text
-          fontSize="20px"
-          fontWeight="700"
-          onClick={() => {
-            handleInputReset();
-            handleSelectSite(siteData.properties._id);
-          }}
-          cursor={'pointer'}
-        >
-          {siteData?.properties?.name}
-        </Text>
-        {canClose && (
-          <Button bgColor="brand.primary" onClick={e => handleSelectSite()}>
-            X
-          </Button>
-        )}
-      </Flex>
-      {siteData?.properties?.tags && <HStack spacing={'4px'} mb={'10px'}>
-       {siteData?.properties?.tags.map(tag => (
-        <Tag key={tag} variantColor='subtle' colorScheme='orange' textTransform={'capitalize'}>
-          {tag}
-        </Tag>
-      ))}
-      </HStack>}
-      <Box cursor={'pointer'}>
-        <Flex
-          w={'auto'}
-          alignItems="center"
-          onClick={() => localService.getLocalGoogleMapsURL(siteData.geometry.coordinates)}
-          whiteSpace={'wrap'}
-        >
-          <Flex gap='6px'> <Box minW={26} minH={26}><MapTrifold size={26} weight="regular" color={'orange'}/></Box> {siteData.properties.address} </Flex>
-        </Flex>
-      </Box>
-
-      {!isSearch && (
+      <SimpleGrid columns={2} spacing={5} templateColumns="73% 25%" gap="2%">
         <Box>
-          <Text mb="2rem" mt="1rem">
-            {siteData.properties.available
-              ? `We still have some free tables`
-              : 'Sorry, there are no tables available'}
-          </Text>
           <Flex justifyContent={'space-between'}>
-            <Button
-              as={NavLink}
-              to={`/detail/${siteData.properties._id}`}
-              variant="secondary2"
+            <Text
+              fontSize="20px"
+              fontWeight="700"
+              onClick={() => {
+                handleInputReset();
+                handleSelectSite(siteData.properties._id);
+              }}
+              cursor={'pointer'}
             >
-              More Info
-            </Button>
-            <Button
-              as={NavLink}
-              to={`/book/${siteData.properties._id}`}
-              variant="primary"
-              isDisabled={!siteData.properties.available}
-            >
-              Book a table
-            </Button>
+              {siteData?.properties?.name}
+            </Text>
+            {canClose && (
+              <Button bgColor="brand.primary" onClick={e => handleSelectSite()}>
+                X
+              </Button>
+            )}
           </Flex>
+          {siteData?.properties?.tags && (
+            <HStack spacing={'4px'} mb={'10px'}>
+              {siteData?.properties?.tags.map(tag => (
+                <Tag
+                  key={tag}
+                  variantColor="subtle"
+                  colorScheme="orange"
+                  textTransform={'capitalize'}
+                >
+                  {tag}
+                </Tag>
+              ))}
+            </HStack>
+          )}
+          <Box cursor={'pointer'}>
+            <Flex
+              w={'auto'}
+              alignItems="center"
+              onClick={() =>
+                localService.getLocalGoogleMapsURL(
+                  siteData.geometry.coordinates
+                )
+              }
+              whiteSpace={'wrap'}
+            >
+              <Flex gap="6px">
+                {' '}
+                <Box minW={26} minH={26}>
+                  <MapTrifold size={26} weight="regular" color={'orange'} />
+                </Box>{' '}
+                {siteData.properties.address}{' '}
+              </Flex>
+            </Flex>
+          </Box>
+
+          {!isSearch && (
+            <Box>
+              <Text mb="2rem" mt="1rem">
+                {siteData.properties.available
+                  ? `We still have some free tables`
+                  : 'Sorry, there are no tables available'}
+              </Text>
+              <Flex justifyContent={'space-between'}>
+                <Button
+                  as={NavLink}
+                  to={`/detail/${siteData.properties._id}`}
+                  variant="secondary2"
+                >
+                  More Info
+                </Button>
+                <Button
+                  onClick={handleBooking}
+                  variant="primary"
+                  isDisabled={!siteData.properties.available}
+                >
+                  Book a table
+                </Button>
+              </Flex>
+            </Box>
+          )}
         </Box>
-      )}
-      </Box>
-      <Box>
-        <Image objectFit='cover' src={siteData.properties.image ||RestaurantDefault} alt="TableIcon" width={'100%'} height={'100%'} borderRadius='5px' />
-      </Box>
-    </SimpleGrid>
+        <Box>
+          <Image
+            objectFit="cover"
+            src={siteData.properties.image || RestaurantDefault}
+            alt="TableIcon"
+            width={'100%'}
+            height={'100%'}
+            borderRadius="5px"
+          />
+        </Box>
+      </SimpleGrid>
     </Box>
   );
 };
