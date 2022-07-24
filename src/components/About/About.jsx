3f5da89ce9426df281951eb5144b278cf50ca316
sergiocl98 <carmona.lorente@bootcamp-upgrade.com>
2022-07-24 22:27:10 +0200

    Responsive cards about

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -46,11 +46,11 @@ const About = () => {
       <Center w="auto" h="auto" mt="30px">
       <Flex alignItems='center' flexDir={'column'} justifyContent='center'>
         <Text fontSize="36px" fontWeight="bold" color="orange"> Our Team </Text>
-        <HStack spacing="20px" mt="10px">
+        <Flex justifyContent='space-between' alignItems={'center'} mt="10px" flexDirection={{base: 'column', md: 'row'}}>
           <ProfileCard name='Guillermo Piñate' img={GuillermoPicture} description='Full Stack Developer, Designer and Photograph' address='Pontevedra, Spain' work='Interfoto' email='gpinate@gmail.com' hashtag='Photograph' hashtagIcon={<Camera size={32} color="#edeef0" weight="thin" />}  />
           <ProfileCard name='Sergio Carmona' img={SergioPicture} description='Full Stack Developer working as Frontend Developer in IoBuilders' address='Madrid, Spain' work='IoBuilders' email='scarmonalorente@gmail.com' hashtag='Gamer' hashtagIcon={<GameController size={32} color="#edeef0" weight="thin" />} />
           <ProfileCard name='Jose Ignacio Labella' img={JosePicture} description='Assistant University Teacher, Full Stack Developer' address='Alicante, Spain' work='University' email='jose@gmail.com' hashtag='Biologist' hashtagIcon={<Bird size={32} color="#edeef0" weight="thin" />} />
-        </HStack>
+        </Flex>
       </Flex>
       </Center>
     </Flex>
