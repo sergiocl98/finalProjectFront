2866691f38776be17185f67263d78619c8d5a135
Jose <ls.joseignacio@gmail.com>
2022-07-25 13:52:28 +0200

    Hace al header comportarse mejor en diferentes resoluciones

--- src/containers/Layout/Topbar/Topbar.jsx
+++ src/containers/Layout/Topbar/Topbar.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
-import { Box, Flex } from '@chakra-ui/react';
+import { Box, Flex, Grid } from '@chakra-ui/react';
 import TopbarSidebarButton from './TopbarSidebarButton';
 import TopbarProfile from './TopbarProfile';
 import TopbarContent from './TopbarContent';
 
 const Topbar = () => {
   return (
-    <Box w='100%' position='fixed' top='0' h='80px' zIndex='101' boxShadow='0px -5px 10px 3px rgba(0,0,0,0.25)' bg='white' color='brand.gray2'>
-      <Flex position='relative' h='80px'>
+    <Box w='100%' position='fixed' top='0' h='80px' zIndex='101' boxShadow='0px -5px 10px 3px rgba(0,0,0,0.25)' bg='white' color='brand.gray2' p="0px 20px">
+      <Grid position='relative' h='80px' templateColumns="1fr auto 1fr">
         <TopbarSidebarButton />
         <TopbarContent />
         <TopbarProfile />
-      </Flex>
+      </Grid>
     </Box>
   );
 };
